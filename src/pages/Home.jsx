c95d9dad5970e19f9373d6eab92e239b49ef81e6
hashirnouman <hashirnouman76@gmail.com>
2023-02-04 22:59:32 +0500

    login done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,23 +9,51 @@ import {
   FormLabel,
   Spacer,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate, redirect } from "react-router-dom";
+import * as api from "../services/auth.api";
 
 const Home = () => {
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const login = async (e) => {
+    e.preventDefault();
+    const response = await api.login({
+      username: username,
+      password: password,
+    });
+    if (response.status === 200) {
+      localStorage.setItem("token", response.data.accessToken);
+      navigate("/page1");
+    }
+  };
   return (
     <div>
       <Center>
         <Box boxShadow="md" width={1000} height={400} p={30}>
           <VStack gap={4}>
             <Text>Welcome to the quiz Sign in</Text>
-            <FormControl gap={3}>
-              <FormLabel>Email address</FormLabel>
-              <Input type="email" />
+            <form onSubmit={(e) => login(e)}>
+              <FormLabel>username</FormLabel>
+              <Input
+                type="text"
+                onChange={(e) => setUserName(e.target.value)}
+                value={username}
+                required
+              />
               <Spacer />
               <FormLabel>Password</FormLabel>
-              <Input type="password" />
-            </FormControl>
-            <Button>Login</Button>
+              <Input
+                type="password"
+                value={password}
+                required
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
+              />
+              <Button type="submit">Login</Button>
+            </form>
           </VStack>
         </Box>
       </Center>
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,4 +1,7 @@
 import axios from "axios";
 export const authInstance = axios.create({
   baseURL: "http://localhost:4000/",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
